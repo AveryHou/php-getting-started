588ebd477b5823d565386394fa238fb4f69082fb
Avery Hou <avery.hou@gmail.com>
2016-10-31 12:00:32 +0800

    composer update

--- web/return-ios.php
+++ web/return-ios.php
@@ -1,10 +1,9 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
+require_once("./parse-conf.php");
 include_once('./AllPay.Payment.Integration.php');
-require_once("parse-conf.php");
 
 use Parse\ParseCloud;
-
 
 $result = "fail";
 
